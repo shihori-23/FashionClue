8952db0ae36e9c1ad4836969505eb0e43087e50f
tsujishihori <shihori.23@i.softbank.jp>
2020-03-29 23:53:00 +0900

    <Add>mixinの導入と性別の表示部分をコンポーネント化

---
+++ resources/js/components/items/GenderComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <span class="genderSpan">{{ gender[genderRole] }}</span>
+</template>
+
+<script>
+export default {
+  props: {
+    genderRole: Number
+  },
+  data() {
+    return {
+      gender: ["レディース", "メンズ"]
+    };
+  },
+  methods: {}
+};
+</script>
+<style scoped>
+</style>
--- resources/js/vuex.js
+++ resources/js/vuex.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
-Vue.use(Vuev);
+Vue.use(Vuex);
 
 export default new Vuex({});
