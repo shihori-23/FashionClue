1f2c3fed88527988c32d574b439f6a59dfe114b7
tsujishihori <shihori.23@i.softbank.jp>
2020-04-03 01:06:13 +0900

    <Add>vue-croppajsの導入・各種CSSの変更

--- resources/js/components/items/FooterComponent.vue
+++ resources/js/components/items/FooterComponent.vue
@@ -1,101 +1,92 @@
-    <template>
-  <footer>
-    <router-link :to="{ name: 'UserHome'}">
-      <v-btn icon>
-        <span class="lnr lnr-home footericon"></span>
-        <!-- <i class="fas fa-home"></i> -->
-      </v-btn>
-    </router-link>
-    <router-link :to="{ name: 'UserBookmark'}">
-      <v-btn icon>
-        <span class="lnr lnr-bookmark footericon"></span>
-        <!-- <i class="far fa-bookmark"></i> -->
-      </v-btn>
-    </router-link>
-    <router-link :to="{ name: 'UserPost'}">
-      <v-btn icon>
-        <span class="lnr lnr-plus-circle footericon"></span>
-        <!-- <i class="far fa-edit"></i> -->
-      </v-btn>
-    </router-link>
-    <router-link :to="{ name: 'UserNotice'}">
-      <v-btn icon @click="isVisibleNoctice = false">
-        <v-badge :value="isVisibleNoctice" overlap dot color="#bc8f8f">
-          <span class="lnr lnr-alarm footericon"></span>
-          <!-- <i class="far fa-bell"></i> -->
-        </v-badge>
-      </v-btn>
-    </router-link>
-    <router-link :to="{ name: 'UserProfile'}">
-      <v-btn icon>
-        <span class="lnr lnr-user footericon"></span>
-        <!-- <i class="fas fa-user"></i> -->
-      </v-btn>
-    </router-link>
-  </footer>
+<template>
+    <footer>
+        <router-link :to="{ name: 'UserHome' }">
+            <v-btn icon active-class="activeIcon">
+                <span class="lnr lnr-home footericon"></span>
+                <!-- <i class="fas fa-home"></i> -->
+            </v-btn>
+        </router-link>
+        <router-link :to="{ name: 'UserBookmark' }">
+            <v-btn icon>
+                <span class="lnr lnr-bookmark footericon"></span>
+                <!-- <i class="far fa-bookmark"></i> -->
+            </v-btn>
+        </router-link>
+        <router-link :to="{ name: 'UserPost' }">
+            <v-btn icon>
+                <span class="lnr lnr-plus-circle footericon"></span>
+                <!-- <i class="far fa-edit"></i> -->
+            </v-btn>
+        </router-link>
+        <router-link :to="{ name: 'UserNotice' }">
+            <v-btn icon @click="isVisibleNoctice = false">
+                <v-badge :value="isVisibleNoctice" overlap dot color="#bc8f8f">
+                    <span class="lnr lnr-alarm footericon"></span>
+                    <!-- <i class="far fa-bell"></i> -->
+                </v-badge>
+            </v-btn>
+        </router-link>
+        <router-link :to="{ name: 'UserProfile' }">
+            <v-btn icon>
+                <span class="lnr lnr-user footericon"></span>
+                <!-- <i class="fas fa-user"></i> -->
+            </v-btn>
+        </router-link>
+    </footer>
 </template>
 <script>
 export default {
-  data() {
-    return {
-      isVisibleNoctice: false
-    };
-  },
-  created() {
-    this.getNoticeData();
-  },
-  mounted() {},
-  methods: {
-    getNoticeData: function() {
-      axios
-        .get("api/notice")
-        .then(res => {
-          console.log(res.data.notice);
-          if (res.data.notice == 0) {
-            this.isVisibleNoctice = false;
-          } else {
-            this.isVisibleNoctice = true;
-          }
-        })
-        .catch(err => console.log(err));
+    data() {
+        return {
+            isVisibleNoctice: false
+        };
+    },
+    created() {
+        this.getNoticeData();
+    },
+    mounted() {},
+    methods: {
+        getNoticeData: function() {
+            axios
+                .get("api/notice")
+                .then(res => {
+                    console.log(res.data.notice);
+                    if (res.data.notice == 0) {
+                        this.isVisibleNoctice = false;
+                    } else {
+                        this.isVisibleNoctice = true;
+                    }
+                })
+                .catch(err => console.log(err));
+        }
     }
-  }
 };
 </script>
 
 <style lang="scss" scoped>
 a {
-  text-decoration: none;
+    text-decoration: none;
 }
 
 footer {
-  position: fixed;
-  bottom: 0;
-  /* background: #ffcc99; */
-  background: #fff;
-  box-shadow: 1px 1px 5px #f0eff0;
-  width: 100%;
-  height: 56px;
-  display: flex;
-  justify-content: space-between;
-  padding: 14px 24px;
-  z-index: 9000;
+    position: fixed;
+    bottom: 0;
+    /* background: #ffcc99; */
+    background: #fff;
+    box-shadow: 1px 1px 5px #f0eff0;
+    width: 100%;
+    height: 56px;
+    display: flex;
+    justify-content: space-between;
+    padding: 14px 24px;
+    z-index: 9000;
 
-  .footericon {
-    font-size: 24px;
-  }
-}
+    .footericon {
+        font-size: 24px;
+    }
 
-i {
-  font-size: 24px;
-  /* font-weight: 200; */
-}
-
-i:hover {
-  color: #bc8f8f;
-}
-
-.mdi-bookmark-outline {
-  font-size: 32px;
+    .footericon:hover {
+        color: #996666;
+    }
 }
 </style>
--- resources/js/components/items/PostBookmarkComponent.vue
+++ resources/js/components/items/PostBookmarkComponent.vue
@@ -1,70 +1,52 @@
 <template>
-  <div>
-    <div class="container">
-      <button class="btn-border" v-if="!isBookmarked" @click.prevent="BookmarkOperationAtClick">
-        <span class="lnr lnr-heart"></span>
-        <!-- <v-icon>mdi-heart-outline</v-icon> -->
-      </button>
-      <button class="btn-border" v-else @click.prevent="removeBookmarkOperationAtClick">
-        <span class="lnr lnr-heart isBookmarkedIcon"></span>
-        <!-- <v-icon color="#ed4956">mdi-heart</v-icon> -->
-      </button>
+    <div>
+        <button
+            class="btn-border bookmarkIcon"
+            v-if="!isBookmarked"
+            @click.prevent="BookmarkOperationAtClick"
+        >
+            <!-- <span class="lnr lnr-heart"></span> -->
+            <v-icon color="#c0c0c0">mdi-heart</v-icon>
+        </button>
+        <button
+            class="btn-border bookmarkIcon"
+            v-else
+            @click.prevent="removeBookmarkOperationAtClick"
+        >
+            <!-- <span class="lnr lnr-heart isBookmarkedIcon"></span> -->
+            <v-icon color="#ed4956">mdi-heart</v-icon>
+        </button>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  props: {
-    post_id: Number,
-    isBookmarked: Boolean
-  },
-  data() {
-    return {
-      originIsBookmarked: this.isBookmarked
-    };
-  },
-  methods: {
-    BookmarkOperationAtClick() {
-      this.$emit("add-post-bookmark");
+    props: {
+        post_id: Number,
+        isBookmarked: Boolean
     },
-    removeBookmarkOperationAtClick() {
-      this.$emit("remove-post-bookmark");
+    data() {
+        return {
+            originIsBookmarked: this.isBookmarked
+        };
+    },
+    methods: {
+        BookmarkOperationAtClick() {
+            this.$emit("add-post-bookmark");
+        },
+        removeBookmarkOperationAtClick() {
+            this.$emit("remove-post-bookmark");
+        }
     }
-    //一旦残しておきます！
-    // addBookmark(id) {
-    // axios
-    //     .post("api/post/post_bookmark/" + id)
-    //     .then(res => {
-    //     console.log(res.data.isBookmarked);
-    //     this.originIsBookmarked = true;
-    //     })
-    //     .catch(err => console.log(err));
-    // },
-
-    // removeBookmark(id) {
-    // axios
-    //     .post("api/destory/post_bookmark/" + id)
-    //     .then(res => {
-    //     console.log(res.data.isBookmarked);
-    //     this.originIsBookmarked = false;
-    //     })
-    //     .catch(err => console.log(err));
-    // }
-    //一旦残しておきます！
-  }
 };
 </script>
 <style scoped>
-a {
-  text-decoration: none;
-}
-
-.isBookmarkedIcon {
-  color: #ed4956;
+.bookmarkIcon {
+    font-size: 24px;
+    padding: 6px 6px 12px 12px;
 }
 
 .btn-border {
-  font-size: 24px;
+    font-size: 24px;
 }
 </style>
